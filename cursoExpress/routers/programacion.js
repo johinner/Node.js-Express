f3f8be4cf992064afe0f6373403fb6cf0e5df7c1
johinner <johimadacor@live.com>
2022-08-24 22:54:03 -0500

    implementando CRUD

--- cursoExpress/routers/programacion.js
+++ cursoExpress/routers/programacion.js
@@ -2,6 +2,8 @@ const express = require('express');
 const {programacion} = require('../datos/cursos.js').infoCursos;
 
 const routerProgramacion = express.Router();
+// Middleware
+routerProgramacion.use(express.json());
 
 routerProgramacion.get('/', (req, res) => {
     return res.send(JSON.stringify(programacion))
@@ -31,9 +33,49 @@ routerProgramacion.get('/:lenguaje/:nivel', (req, res) => {
 
     if(resultados.length === 0){
         return res.status(404).send(`No se encontraron cursos de ${lenguaje} de nivel ${nivel}`);
+        // return res.status(404).end(); // respuesta vacia.
     }
 
     res.send(JSON.stringify(resultados));
 });
 
+routerProgramacion.post('/', (req, res) => {
+    let cursoNuevo = req.body;
+    programacion.push(cursoNuevo);
+    res.send(JSON.stringify(programacion));
+});
+
+routerProgramacion.put('/:id', (req, res) => {
+    const cursoActualizado = req.body;
+    const id = req.params.id;
+
+    const indice = programacion.findIndex(curso => curso.id == id);
+    if(indice >= 0){
+        programacion[indice] = cursoActualizado;
+    }
+    res.send(JSON.stringify(programacion));
+})
+
+routerProgramacion.patch('/:id', (req, res) => {
+    const infoActualizada = req.body;
+    const id = req.params.id;
+    
+    const indice = programacion.findIndex(curso => curso.id == id);
+
+    if(indice >= 0){
+        const cursoModificar = programacion[indice];
+        Object.assign(cursoModificar, infoActualizada)
+    }
+    res.send(JSON.stringify(programacion));
+});
+
+routerProgramacion.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    const indice = programacion.findIndex(curso => curso.id == id);
+
+    if(indice >= 0){
+        programacion.splice(indice, 1);
+    }
+    res.send(JSON.stringify(programacion));
+})
 module.exports = routerProgramacion;
